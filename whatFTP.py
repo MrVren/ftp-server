cf1d9b7125122cc8ed5f78b5b14c9b864efa78f4
Alexander Konovalov <49760035+MrVren@users.noreply.github.com>
2022-06-07 00:55:34 +0300

    Update whatFTP.py

--- whatFTP.py
+++ whatFTP.py
@@ -4,12 +4,10 @@ from pyftpdlib.authorizers import DummyAuthorizer, AuthenticationFailed
 from pyftpdlib.servers import FTPServer
 from pyftpdlib.handlers import FTPHandler
 
-from plyer import notification 
-
 
 
 def main():
-    PATH = 'D:\\'
+    PATH = '.'
     os.chdir(PATH)
     ip = socket.gethostbyname(socket.gethostname())
     addr = (ip, 21)
@@ -39,51 +37,17 @@ def main():
     server.max_cons = 10
     server.max_cons_per_ip = 5
 
-    show_notify('FTP Server', 'Server started')
 
     server.serve_forever()
 
 
-def show_notify(title, message):
-    notification.notify(title = title, 
-                        message = message, 
-                        timeout = 5)
-
-
 
 class Events(FTPHandler):
-    def on_login_failed(self, username, password):
-        show_notify('FTP Server', 'Log in failed\nname: ' + 
-                    username + '\npassword: ' + password) 
-        
+
     def on_incomplete_file_received(self, file):
-        show_notify('FTP Server', file+' is not recieved')
         import os
         os.remove(file)
-'''
-    def on_connect(self):
-        show_notify('FTP Server', 'user connected')
-    
-    def on_disconnect(self):
-        show_notify('FTP Server', 'user disconnected')
 
-    def on_login(self, username):
-        show_notify('FTP Server', 'user ' + username + ' log in')
-'''
-    
-'''
-    def on_logout(self, username):
-        show_notify('FTP Server', 'user log out')
-
-    def on_file_sent(self, file):
-        show_notify('FTP Server', file+' was sent')
-
-    def on_file_received(self, file):
-        show_notify('FTP Server', file+' was recieved')
-
-    def on_incomplete_file_sent(self, file):
-        show_notify('FTP Server', file+' is not sent')
-'''
     
 
 
